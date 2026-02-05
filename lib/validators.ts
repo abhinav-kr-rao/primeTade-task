aea4179b5e2370c0d42a9b4eba04d64cb16ae84a
Abhinav Kumar Rao <shashankr212@gmail.com>
2026-02-05 23:04:29 +0530

    zod errors fixed

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-    email: z.string().email('Invalid email address'),
+    email: z.email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
     role: z.enum(['USER', 'ADMIN']).optional(),
 });
 
 export const loginSchema = z.object({
-    email: z.string().email('Invalid email address'),
+    email: z.email('Invalid email address'),
     password: z.string().min(1, 'Password is required'),
 });
 
